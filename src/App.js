66b92780fc008985259f43189b97256775afcb35
ismahan jamea <ismahanjamea@gmail.com>
2020-12-16 23:23:44 -0800

    another change to about me

--- src/App.js
+++ src/App.js
@@ -3,19 +3,35 @@ import NavBar from "./components/Navbar/Navbar"
 import Title from "./components/TypeText/Title"
 import Carousel from "./components/Carousel/Carousel"
 import About from "./pages/About/About"
+import { Parallax, Background } from "react-parallax"
+import Fade from "react-reveal/Fade"
+import Container from "react-bootstrap/Container"
+import Image1 from "./assets/background.jpg"
+
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{position: "relative"}}>
      <NavBar/> 
      <Carousel/>
       <Title/>
+      <div>
+      <Parallax
+        blur={{ min: -30, max: 30 }}
+        bgImage={Image1}
+        bgImageAlt="lights"
+        strength={-200}
+    >
+        <div>
+      <Container className="container-box rounded">
+        <Fade duration={500}>
       <About/>
-  
-
-    
-       
+      </Fade>
+      </Container>
+      </div>
+      </Parallax>
+      </div>  
     </div>
   );
 }
